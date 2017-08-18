cd1b597de281e75db91f877ffd331884879415fe
Mathew <hunter3142@gmail.com>
2017-08-17 21:41:02 -0400

    Initialize and Authorize

---
+++ lib/kele.rb
@@ -0,0 +1,18 @@
+require 'httparty'
+
+class Bloc
+	include HTTParty
+
+	base_uri 'https://www.bloc.io/api/v1'
+
+	def initialize (u, p)
+		@auth_token = get_auth_token(u, p)
+	end
+
+	private
+	def get_auth_token(u, p)
+		response = self.class.post("/sessions", body: {email: u, password: p})
+		raise StandardError if response.code != 200
+		response["auth_token"]
+	end
+end
