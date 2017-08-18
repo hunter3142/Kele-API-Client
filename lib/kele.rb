0fcad5c9ff20ab98af5a14ba781abb5943421738
Mathew <hunter3142@gmail.com>
2017-08-17 22:42:07 -0400

    Retrieve users

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,17 +1,23 @@
 require 'httparty'
+require 'json'
 
 class Kele
 	include HTTParty
 
 	base_uri 'https://www.bloc.io/api/v1'
 
-	def initialize (u, p)
-		@auth_token = get_auth_token(u, p)
+	def initialize (e, p)
+		@auth_token = get_auth_token(e, p)
+	end
+
+	def get_me
+		response = self.class.get("/users/me", headers: { "authorization" => @auth_token })
+		@user_data = JSON.parse(response.body)
 	end
 
 	private
-	def get_auth_token(u, p)
-		response = self.class.post("/sessions", body: {email: u, password: p})
+	def get_auth_token(e, p)
+		response = self.class.post("/sessions", body: {email: e, password: p})
 		raise StandardError if response.code != 200
 		response["auth_token"]
 	end
